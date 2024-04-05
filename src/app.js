249e4cc63366541476cc777af9351a1f3c4e1c36
Knightlife27 <143671996+Knightlife27@users.noreply.github.com>
2024-04-05 21:27:44 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function createExcuse() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const partOne = who[Math.floor(Math.random() * who.length)];
+
+  const partTwo = action[Math.floor(Math.random() * action.length)];
+
+  const partThree = what[Math.floor(Math.random() * what.length)];
+
+  const partFour = when[Math.floor(Math.random() * when.length)];
+
+  let completeSentence = `${partOne} ${partTwo} ${partThree} ${partFour}`;
+
+  return completeSentence;
+}
+
+document.getElementById("excuse").innerHTML = createExcuse();
